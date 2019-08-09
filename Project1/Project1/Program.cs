236fa0c7db3828ba84acf769ca8da869d2afc39e
callofduty890 <1032031912@qq.com>
2019-08-10 01:51:10 +0800

    添加了加法,并且显示加出来的效果

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -10,6 +10,10 @@ namespace Project1
             Console.WriteLine("Hello World!");
             //显示控制信息
             Console.WriteLine("测试程序");
+            //进行加法运算
+            int a = 1 + 2;
+            //答应显示结果
+            Console.WriteLine("1+2=" + a);
             //等待读取信息
             Console.ReadLine();
         }
