bbb9c1511595b19a092f0d7ee61ca92559b9a6bd
callofduty890 <1032031912@qq.com>
2019-08-10 01:22:18 +0800

    加强了注释

---
+++ Project1/Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //显示控制信息
+            Console.WriteLine("Hello World!");
+            //显示控制信息
+            Console.WriteLine("测试程序");
+            //等待读取信息
+            Console.ReadLine();
+        }
+    }
+}
